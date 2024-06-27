8fceea978deb12094ace9827112eba1594e970e4
Minh Triet <97509338+MinhTrietTran@users.noreply.github.com>
2024-06-27 22:34:18 +0700

    Upload documents and freeze this project

--- sql/ACTIVATEAUDIT_INCDB.sql
+++ sql/ACTIVATEAUDIT_INCDB.sql
@@ -1,7 +1,7 @@
 -- ==============AUDIT==============
 
 -- Kích ho?t vi?c ghi nh?t ký h? th?ng
-ALTER SYSTEM SET audit_trail = db, EXTENDED SCOPE = SPFILE;
+ALTER SYSTEM SET audit_trail = 'db' SCOPE = SPFILE;
 
 -- Kh?i ??ng l?i c? s? d? li?u ?? thay ??i có hi?u l?c
 SHUTDOWN IMMEDIATE;
